d0b3dfbe1c1a939bf21d33ebd3bec881b850d451
davisc0801 <davisc1014@gmail.com>
2019-03-27 09:08:13 -0600

    Complete spec for character class

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -2,4 +2,20 @@ require "./lib/character"
 
 describe Character do
 
+  before do
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+  end
+
+  it "has a name" do
+    expect(@kitt.name).to eq("KITT")
+  end
+
+  it "has an actor" do
+    expect(@kitt.actor).to eq("William Daniels")
+  end
+
+  it "has a salary" do
+    expect(@kitt.salary).to eq(1000000)
+  end
+
 end
