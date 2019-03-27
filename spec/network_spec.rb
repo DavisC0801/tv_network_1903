0977ef00d9a45e0ba64cd69c48d528ae50822335
davisc0801 <davisc1014@gmail.com>
2019-03-27 09:36:58 -0600

    complete spec for network class

--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -8,4 +8,33 @@ describe Network do
     @nbc = Network.new("NBC")
   end
 
+  it "has a name" do
+    expect(@nbc.name).to eq("NBC")
+  end
+
+  it "has no shows by default" do
+    expect(@nbc.shows).to eq([])
+  end
+
+  context "add shows and actors" do
+
+    before do
+      @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+      @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+      @knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+      @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+      @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+      @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+      @nbc.add_show(@knight_rider)
+      @nbc.add_show(@parks_and_rec)
+    end
+
+    it "stores shows that have been added" do
+      expect(@nbc.shows).to eq([@knight_rider, @parks_and_rec])
+    end
+
+    it "finds the highest paid actor in the network" do
+      expect(@nbc.highest_paid_actor).to eq("Amy Poehler")
+    end
+  end
 end
