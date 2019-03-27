19355d86ce3899d8a1d7c69569154b7af61acc53
davisc0801 <davisc1014@gmail.com>
2019-03-27 09:48:31 -0600

    Complete network basic functions

--- lib/network.rb
+++ lib/network.rb
@@ -1,3 +1,21 @@
 class Network
 
+  attr_reader :name, :shows
+
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def highest_paid_actor
+    hi = 0
+    @shows.each do |show|
+      hi = show.characters.max_by{|character| character.salary}
+    end
+    hi.actor
+  end
 end
--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -21,10 +21,10 @@ describe Network do
     before do
       @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
       @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
-      @knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+      @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
       @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
       @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
-      @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+      @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
       @nbc.add_show(@knight_rider)
       @nbc.add_show(@parks_and_rec)
     end
